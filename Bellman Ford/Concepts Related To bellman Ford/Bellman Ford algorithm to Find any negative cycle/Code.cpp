b5fd32ee8bc7871dfb645a82ea52629e31f4d8ed
Jagonmoy Dey <jagonmoy25@student.sust.edu>
2020-10-04 17:42:54 +0600

    Create Code.cpp

---
+++ Bellman Ford/Concepts Related To bellman Ford/Bellman Ford algorithm to Find any negative cycle/Code.cpp
@@ -0,0 +1,41 @@
+ll vertex ;
+VL dis,parent ;
+vector < pair < PLL,ll > > graphedge ;
+void bellmanford()
+{
+    dis.clear() ;
+    dis.resize(vertex+5,0);
+    parent.clear();
+    parent.resize(vertex+5,-1);
+    ll x ;
+    INC(i,1,vertex)
+    {
+        x = -1;
+        INC(j,0,graphedge.size()-1) {
+            ll startnode = graphedge[j].F.F ;
+            ll endnode = graphedge[j].F.S ;
+            ll weight = graphedge[j].S ;
+            if (dis[startnode] + weight < dis[endnode])
+                {
+                    dis[endnode] = max(neginf,dis[startnode] + weight);
+                    parent[endnode] = startnode;
+                    x = endnode ;
+                }
+        }
+    }
+    if ( x == -1 ) cout << "No negative cycle in this graph "<< endl;
+    else {
+        INC(i,1,vertex) x = parent[x];
+        VL path ;
+        ll cur = x ;
+        while(1) {
+            path.PB(cur);
+            cur = parent[cur] ;
+            if ( cur == x ) break ;
+        }
+        reverse (path.begin(), path.end());
+        cout << "Negative cycle: ";
+        INC(i,0,path.size()-1) cout << path[i] << ' ';
+        cout << endl;
+    }
+}
